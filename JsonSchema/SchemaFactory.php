api-platform/hydra

fix(json-schema): factory backward compatibility layer (#4758)
* fix(json-schema): factory backward compatibility layer

link #4753

* sdkjhg

* php 7...

* php 7...

* php 7...

* php 7...

* fix

* tired

* come on

* come on

JsonSchema/SchemaFactory.php
@@ -17,6 +17,7 @@ use ApiPlatform\JsonLd\ContextBuilder;
 use ApiPlatform\JsonSchema\Schema;
 use ApiPlatform\JsonSchema\SchemaFactory as BaseSchemaFactory;
 use ApiPlatform\JsonSchema\SchemaFactoryInterface;
+use ApiPlatform\Metadata\Operation;
 
 /**
  * Decorator factory which adds Hydra properties to the JSON Schema document.
@@ -70,9 +71,9 @@ final class SchemaFactory implements SchemaFactoryInterface
     /**
      * {@inheritdoc}
      */
-    public function buildSchema(string $className, string $format = 'jsonld', string $type = Schema::TYPE_OUTPUT, ?string $operationType = null, ?string $operationName = null, ?Schema $schema = null, ?array $serializerContext = null, bool $forceCollection = false): Schema
+    public function buildSchema(string $className, string $format = 'jsonld', string $type = Schema::TYPE_OUTPUT, ?Operation $operation = null, ?Schema $schema = null, ?array $serializerContext = null, bool $forceCollection = false): Schema
     {
-        $schema = $this->schemaFactory->buildSchema($className, $format, $type, $operationType, $operationName, $schema, $serializerContext, $forceCollection);
+        $schema = $this->schemaFactory->buildSchema($className, $format, $type, $operation, $schema, $serializerContext, $forceCollection);
         if ('jsonld' !== $format) {
             return $schema;
         }
@@ -187,5 +188,3 @@ final class SchemaFactory implements SchemaFactoryInterface
         }
     }
 }
-
-class_alias(SchemaFactory::class, \ApiPlatform\Core\Hydra\JsonSchema\SchemaFactory::class);

api-platform/json-hal

JsonSchema/SchemaFactory.php
@@ -15,6 +15,7 @@ namespace ApiPlatform\Hal\JsonSchema;
 
 use ApiPlatform\JsonSchema\Schema;
 use ApiPlatform\JsonSchema\SchemaFactoryInterface;
+use ApiPlatform\Metadata\Operation;
 
 /**
  * Decorator factory which adds HAL properties to the JSON Schema document.
@@ -56,9 +57,9 @@ final class SchemaFactory implements SchemaFactoryInterface
     /**
      * {@inheritdoc}
      */
-    public function buildSchema(string $className, string $format = 'jsonhal', string $type = Schema::TYPE_OUTPUT, ?string $operationType = null, ?string $operationName = null, ?Schema $schema = null, ?array $serializerContext = null, bool $forceCollection = false): Schema
+    public function buildSchema(string $className, string $format = 'jsonhal', string $type = Schema::TYPE_OUTPUT, ?Operation $operation = null, ?Schema $schema = null, ?array $serializerContext = null, bool $forceCollection = false): Schema
     {
-        $schema = $this->schemaFactory->buildSchema($className, $format, $type, $operationType, $operationName, $schema, $serializerContext, $forceCollection);
+        $schema = $this->schemaFactory->buildSchema($className, $format, $type, $operation, $schema, $serializerContext, $forceCollection);
         if ('jsonhal' !== $format) {
             return $schema;
         }
@@ -135,5 +136,3 @@ final class SchemaFactory implements SchemaFactoryInterface
         }
     }
 }
-
-class_alias(SchemaFactory::class, \ApiPlatform\Core\Hal\JsonSchema\SchemaFactory::class);